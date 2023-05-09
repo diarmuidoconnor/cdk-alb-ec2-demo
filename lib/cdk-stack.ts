6a5fcd8d699246303e8d851f518d031f1260e832
Diarmuid O Connor <doconnor@wit.ie>
2023-05-09 17:34:41 +0100

    Cleanup stack

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,40 +1,50 @@
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as cdk from 'aws-cdk-lib';
+import {AutoScalingGroup} from 'aws-cdk-lib/aws-autoscaling';
+import {
+  Vpc,
+  SubnetType,
+  SecurityGroup,
+  UserData,
+  InstanceClass,
+  InstanceSize,
+  AmazonLinuxImage,
+  AmazonLinuxGeneration,
+  InstanceType,
+  Peer,
+  Port,
+} from 'aws-cdk-lib/aws-ec2';
+import {App, CfnOutput, Duration, Stack, StackProps} from 'aws-cdk-lib';
 import {readFileSync} from 'fs';
-import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import {
+  ApplicationProtocol,
+  ApplicationLoadBalancer,
+  ListenerCondition,
+  ListenerAction,
+} from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'vpc',{
-      cidr: '10.0.0.0/16',
+    const vpc = new Vpc(this, 'vpc', {
+      // cidr: '10.0.0.0/16',
       natGateways: 1,
-      subnetConfiguration: [
-        {name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC},
-      ],
-    })
+      // subnetConfiguration: [
+      //   {name: 'public', cidrMask: 24, subnetType: SubnetType.PUBLIC},
+      // ],
+    });
 
-    const serverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+    const serverSG = new SecurityGroup(this, 'webserver-sg', {
       vpc,
       allowAllOutbound: true,
     });
 
     // serverSG.addIngressRule(
-    //   ec2.Peer.anyIpv4(),
-    //   ec2.Port.tcp(22),
+    //   Peer.anyIpv4(),
+    //   Port.tcp(22),
     //   'allow SSH access from anywhere',
     // );
 
-    // serverSG.addIngressRule(
-    //   ec2.Peer.anyIpv4(),
-    //   ec2.Port.tcp(3000),
-    //   'allow HTTP traffic from anywhere',
-    // );
-
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
+    const alb = new ApplicationLoadBalancer(this, 'alb', {
       vpc,
       internetFacing: true,
     });
@@ -44,31 +54,25 @@ export class CdkStack extends cdk.Stack {
       open: true,
     });
 
-    // const userData = ec2.UserData.forLinux();
-    // userData.addCommands(
-    //   'sudo su',
-    //   'yum install -y httpd',
-    //   'systemctl start httpd',
-    //   'systemctl enable httpd',
-    //   'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
-    // );
+    const userDataScript = readFileSync('./lib/user-data-API.sh', 'utf8');
 
-     const userDataScript = readFileSync('./lib/user-data-API.sh', 'utf8');
-
-    const userData = ec2.UserData.forLinux();
+    const userData = UserData.forLinux()
     userData.addCommands(userDataScript);
 
-    const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+    const asg = new AutoScalingGroup(this, 'asg', {
       vpc,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.BURSTABLE2,
-        ec2.InstanceSize.MICRO,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      instanceType: InstanceType.of(
+        InstanceClass.BURSTABLE2,
+        InstanceSize.MICRO,
       ),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      machineImage: new AmazonLinuxImage({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
       securityGroup: serverSG,
-      keyName: 'ec2-key-pair',
+      // keyName: 'ec2-key-pair',
       userData,
       minCapacity: 1,
       maxCapacity: 3,
@@ -82,14 +86,14 @@ export class CdkStack extends cdk.Stack {
         path: '/',
         unhealthyThresholdCount: 2,
         healthyThresholdCount: 5,
-        interval: cdk.Duration.seconds(30),
+        interval: Duration.seconds(30),
       },
     });
 
     listener.addAction('/static', {
       priority: 5,
-      conditions: [elbv2.ListenerCondition.pathPatterns(['/static'])],
-      action: elbv2.ListenerAction.fixedResponse(200, {
+      conditions: [ListenerCondition.pathPatterns(['/static'])],
+      action: ListenerAction.fixedResponse(200, {
         contentType: 'text/html',
         messageBody: '<h1>Static ALB Response</h1>',
       }),
@@ -103,7 +107,7 @@ export class CdkStack extends cdk.Stack {
       targetUtilizationPercent: 75,
     });
 
-    new cdk.CfnOutput(this, 'albDNS', {
+    new CfnOutput(this, 'albDNS', {
       value: alb.loadBalancerDnsName,
     });
   }
